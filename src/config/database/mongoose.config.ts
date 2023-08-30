a8cd9c3f72ef341b6e1b655577786a6828ab348d
Alireza zamani <zamani802000@gmail.com>
2023-08-31 01:52:50 +0330

    create global enum and fix config mongoose

---
+++ src/config/database/mongoose.config.ts
@@ -0,0 +1,17 @@
+import { NodeEnviorment } from 'src/common/enums/global.enum';
+export function getMongoURL() {
+  const {
+    NODE_ENV,
+    MONGO_DATABSE,
+    MONGO_HOST,
+    MONGO_PASSWORD,
+    MONGO_PORT,
+    MONGO_USERNAME,
+  } = process.env;
+
+  const connection =
+    NODE_ENV == NodeEnviorment.DEV
+      ? `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABSE}`
+      : 'mongodb://alireza:zamani2020@127.0.0.1:27017/webidemy_db?directConnection=true&serverSelectionTimeoutMS=2000&authSource=admin';
+  return connection;
+}
