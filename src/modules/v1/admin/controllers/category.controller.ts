ccd07894ce5f16169a73ed5cb6c619641a88f2e5
Alireza zamani <zamani802000@gmail.com>
2023-09-07 22:42:03 +0330

    create edit category route

--- src/modules/v1/admin/controllers/category.controller.ts
+++ src/modules/v1/admin/controllers/category.controller.ts
@@ -1,14 +1,18 @@
 import {
   Body,
   Controller,
-
+  Delete,
+  Get,
   HttpCode,
   HttpStatus,
+  Param,
   Post,
+  Query,
 } from '@nestjs/common';
 import { Auth } from 'src/common/decorators/Auth.decorator';
 import { createCategoryDTO } from '../dto/admin.dto';
 import { CategoryService } from '../../category/category.service';
+import { BasePaginateDTO } from 'src/common/dtos/base-paginate.dto';
 @Auth()
 @Controller({
   path: '/admin/categories',
@@ -20,5 +24,29 @@ export class CategoryController {
   @Post('create')
   async create(@Body() categoryDTO: createCategoryDTO) {
     return await this.categoryService.store(categoryDTO);
+  }
+  @HttpCode(HttpStatus.OK)
+  @Get()
+  async Index(@Query() BasePaginateDTO: BasePaginateDTO) {
+    return this.categoryService.showCategoriesInAdminPanel(BasePaginateDTO);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Delete(':cateId')
+  async Delete(@Param('cateId') cateId: string) {
+    return await this.categoryService.destroy(cateId);
+  }
+  @HttpCode(HttpStatus.OK)
+  @Get('edit/:cateId')
+  async Edit(@Param('cateId') cateId: string) {
+    return await this.categoryService.edit(cateId);
+  }
+  @HttpCode(HttpStatus.OK)
+  @Get('edit/:cateId')
+  async Update(
+    @Param('cateId') cateId: string,
+    categoryDTO: createCategoryDTO,
+  ) {
+    return await this.categoryService.update(cateId, categoryDTO);
   }
 }
--- src/modules/v1/category/category.service.ts
+++ src/modules/v1/category/category.service.ts
@@ -1,15 +1,40 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { PaginateModel } from 'mongoose';
 import { Category } from 'src/common/interfaces/category.interface';
 import { createCategoryDTO } from '../admin/dto/admin.dto';
 import { Messages } from 'src/common/enums/message.enum';
+import { BasePaginateDTO } from 'src/common/dtos/base-paginate.dto';
+import isMongoId from 'validator/lib/isMongoId';
 
 @Injectable()
 export class CategoryService {
   constructor(
     @InjectModel('Category') private categoryModel: PaginateModel<Category>,
   ) {}
+
+  async showCategoriesInAdminPanel(BasePaginateDTO: BasePaginateDTO) {
+    const { page, item_count } = BasePaginateDTO;
+    const categories = await this.categoryModel.paginate(
+      {},
+      {
+        page,
+        limit: item_count,
+        sort: { createdAt: -1 },
+      },
+    );
+
+    return {
+      data: categories.docs,
+      limit: categories.limit,
+      page: categories.page,
+      pages: categories.pages,
+    };
+  }
 
   async store(categoryDTO: createCategoryDTO) {
     const { title } = categoryDTO;
@@ -24,6 +49,33 @@ export class CategoryService {
     return {
       status: 'messages',
       message: 'the Category has been created!',
+    };
+  }
+  async edit(cateId: string) {
+    if (!isMongoId) throw new BadRequestException('the cate id is not true');
+    const cate = await this.categoryModel.findById(cateId);
+    if (!cate) throw new NotFoundException('the category is not found!');
+
+    return cate;
+  }
+
+  async update(cateId: string, categoryDTO: createCategoryDTO) {
+    if (!isMongoId) throw new BadRequestException('the cate id is not true');
+    const cate = await this.categoryModel.findById(cateId);
+    if (!cate) throw new NotFoundException('the category is not found!');
+
+    await cate.updateOne({ $set: { ...categoryDTO } });
+    return {
+      status: 'success',
+    };
+  }
+  async destroy(cateId: string) {
+    if (!isMongoId) throw new BadRequestException('the cate id is not true');
+    const cate = await this.categoryModel.findById(cateId);
+    if (!cate) throw new NotFoundException('the category is not found!');
+    cate.deleteOne();
+    return {
+      status: 'success',
     };
   }
 }
