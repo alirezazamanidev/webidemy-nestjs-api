20d2c81006646cb5b364739b9183df56693738e3
alireza zamani <alirezazamanidev80@gmail.com>
2023-11-15 20:41:40 +0000

    create Blog interface

---
+++ src/common/interfaces/blog.interface.ts
@@ -0,0 +1,17 @@
+import { Document } from "mongoose";
+import { User } from "./user.interface";
+
+export interface Blog extends Document {
+    id: string;
+    author: User;
+    title: string;
+    isPublished: boolean
+    slug: string
+    studyTime: string
+    images: {}
+    likeCount: number;
+
+    viewCount: number;
+    commentCount: number;
+
+}
