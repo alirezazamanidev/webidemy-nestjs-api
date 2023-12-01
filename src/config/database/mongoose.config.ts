81c5682bf9701ec992e87ee1506cdc21838ab1fd
Alireza <101404857+alirezazmpro@users.noreply.github.com>
2023-12-01 18:21:41 +0330

    Update mongoose.config.ts

--- src/config/database/mongoose.config.ts
+++ src/config/database/mongoose.config.ts
@@ -5,7 +5,7 @@ export function getMongoURL() {
   const connection =
     NODE_ENV == NodeEnviorment.DEV
       ? `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABSE}`
-      : 'mongodb://zamani2020@127.0.0.1:27017/webidemydb?directConnection=true&serverSelectionTimeoutMS=2000&authSource=admin';
+      : 'mongodb://pachim:zamani2020@127.0.0.1:27017/webidemydb?directConnection=true&serverSelectionTimeoutMS=2000&authSource=admin';
 
       
   return connection;
